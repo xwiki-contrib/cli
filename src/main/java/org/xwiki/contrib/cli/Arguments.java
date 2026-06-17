d9f2931b09921601acd6baee4aa10f70826295d1
Josué Tille <josue.tille@xwiki.com>
2026-06-17 11:14:35 +0200

    CLI-29: Rework parameter for better consistentcy

--- src/main/java/org/xwiki/contrib/cli/Arguments.java
+++ src/main/java/org/xwiki/contrib/cli/Arguments.java
@@ -21,6 +21,8 @@
 package org.xwiki.contrib.cli;
 
 import ch.qos.logback.classic.Level;
+
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -51,13 +53,77 @@ public class Arguments
         }
         while (i < args.length) {
             switch (args[i]) {
-                case "-c", "--configuration" ->  {
+                case "--help", "-help", "-h", "help" -> cmd.setAction(Command.Action.HELP);
+                case "-c", "--configuration" -> {
                     try {
                         readConfigFile(getNextParameter(args, i++), cmd);
                     } catch (IOException e) {
                         throw new CommandException("Could not read config file", e);
                     }
                 }
+                case "--edit-page" -> cmd.setAction(Command.Action.EDIT_PAGE);
+                case "--get-content" -> cmd.setAction(Command.Action.GET_CONTENT);
+                case "--set-content" -> {
+                    cmd.setContent(getNextParameter(args, i++));
+                    cmd.setAction(Command.Action.SET_CONTENT);
+                }
+                case "--edit-content" -> cmd.setAction(Command.Action.EDIT_CONTENT);
+                case "--edit-macro" -> {
+                    cmd.setMacro(getNextParameter(args, i++));
+                    cmd.setAction(Command.Action.EDIT_MACRO);
+                }
+                case "--get-title" -> cmd.setAction(Command.Action.GET_TITLE);
+                case "--set-title" -> {
+                    cmd.setTitle(getNextParameter(args, i++));
+                    cmd.setAction(Command.Action.SET_TITLE);
+                }
+                case "--list-properties" -> cmd.setAction(Command.Action.LIST_PROPERTIES);
+                case "--list-objects" -> cmd.setAction(Command.Action.LIST_OBJECTS);
+                case "--get-property" -> {
+                    cmd.setProperty(getNextParameter(args, i++));
+                    cmd.setAction(Command.Action.GET_PROPERTY_VALUE);
+                }
+                case "--set-property" -> {
+                    cmd.setProperty(getNextParameter(args, i++));
+                    cmd.setAction(Command.Action.SET_PROPERTY_VALUE);
+                }
+                case "--edit-property" -> {
+                    cmd.setProperty(getNextParameter(args, i++));
+                    cmd.setAction(Command.Action.EDIT_PROPERTY);
+                }
+                case "--list-attachments" -> cmd.setAction(Command.Action.LIST_ATTACHMENTS);
+                case "--mount" -> {
+                    cmd.setMountPath(getNextParameter(args, i++));
+                    cmd.setAction(Command.Action.MOUNT);
+                }
+                case "--sync-daemon" -> {
+                    cmd.setAction(Command.Action.SYNC);
+                }
+                case "--pull-page" -> {
+                    cmd.setPullReference(getNextParameter(args, i++));
+                    cmd.setAction(Command.Action.PULL_PAGE);
+                }
+                case "--push-page" -> {
+                    cmd.setPushReference(getNextParameter(args, i++));
+                    cmd.setAction(Command.Action.PUSH_PAGE);
+                }
+                case "--pull-all-pages" -> {
+                    cmd.setAction(Command.Action.PULL_ALL_PAGES);
+                }
+                case "--push-all-pages" -> {
+                    cmd.setAction(Command.Action.PUSH_ALL_PAGES);
+                }
+
+                case "--loglevel" -> cmd.setLogLevel(getNextParameter(args, i++));
+                case "--print-xml" -> cmd.setPrintXML(true);
+                case "-H" -> {
+                    String[] header = HEADER_SPLIT_PATTERN.split(getNextParameter(args, i));
+                    headers.put(header[0], header[1]);
+                }
+                case "-u", "--url" -> cmd.setUrl(getNextParameter(args, i++));
+                case "-w" -> cmd.setWiki(getNextParameter(args, i++));
+
+                case "--editor" -> cmd.setEditor(getNextParameter(args, i++));
                 case "-p" -> cmd.setPage(getNextParameter(args, i++));
                 case "-o" -> {
                     var objectParts = getNextParameter(args, i).split("/");
@@ -73,72 +139,27 @@ public class Arguments
                     }
                     i++;
                 }
-                case "-w" -> cmd.setWiki(getNextParameter(args, i++));
                 case "-v" -> cmd.setValue(getNextParameter(args, i++));
-                case "--editor" -> cmd.setEditor(getNextParameter(args, i++));
-                case "--pom" -> cmd.setPom(true);
-                case "-H" -> {
-                    String[] header = HEADER_SPLIT_PATTERN.split(getNextParameter(args, i));
-                    headers.put(header[0], header[1]);
-                }
-                case "--user" -> cmd.setUser(getNextParameter(args, i++));
-                case "--pass" -> cmd.setPass(getNextParameter(args, i++));
-                case "--wiki-readonly" -> cmd.setWikiReadonly(true);
-                case "--wiki-writeonly" -> cmd.setWikiWriteonly(true);
-                case "--write-to-xml" -> cmd.setOutputFile(getNextParameter(args, i++));
+                case "--property" -> cmd.setProperty(getNextParameter(args, i++));
                 case "--read-from-xml" -> cmd.setInputFile(getNextParameter(args, i++));
-                case "--write-to-mvn-repository" -> cmd.setXmlWriteDir(getNextParameter(args, i++));
+                case "--write-to-xml" -> cmd.setOutputFile(getNextParameter(args, i++));
                 case "--xml-file" -> {
                     cmd.setInputFile(getNextParameter(args, i++));
                     cmd.setOutputFile(cmd.inputFile());
                 }
-                case "--sync-data-source" -> cmd.setSyncDataSource(getNextParameter(args, i++));
-                case "-u", "--url" -> cmd.setUrl(getNextParameter(args, i++));
-                case "--edit-page" -> cmd.setAction(Command.Action.EDIT_PAGE);
-                case "--edit-content" -> cmd.setAction(Command.Action.EDIT_CONTENT);
-                case "--edit-macro" -> {
-                    cmd.setMacro(getNextParameter(args, i++));
-                    cmd.setAction(Command.Action.EDIT_MACRO);
-                }
-                case "--list-properties" -> cmd.setAction(Command.Action.LIST_PROPERTIES);
-                case "--list-objects" -> cmd.setAction(Command.Action.LIST_OBJECTS);
-                case "--list-attachments" -> cmd.setAction(Command.Action.LIST_ATTACHMENTS);
-                case "--get-content" -> cmd.setAction(Command.Action.GET_CONTENT);
-                case "--get-title" -> cmd.setAction(Command.Action.GET_TITLE);
-                case "--set-content" -> {
-                    cmd.setContent(getNextParameter(args, i++));
-                    cmd.setAction(Command.Action.SET_CONTENT);
-                }
-                case "--set-title" -> {
-                    cmd.setTitle(getNextParameter(args, i++));
-                    cmd.setAction(Command.Action.SET_TITLE);
-                }
-                case "--get-property" -> {
-                    cmd.setProperty(getNextParameter(args, i++));
-                    cmd.setAction(Command.Action.GET_PROPERTY_VALUE);
-                }
-                case "--set-property" -> {
-                    cmd.setProperty(getNextParameter(args, i++));
-                    cmd.setAction(Command.Action.SET_PROPERTY_VALUE);
-                }
-                case "--edit-property" -> {
-                    cmd.setProperty(getNextParameter(args, i++));
-                    cmd.setAction(Command.Action.EDIT_PROPERTY);
-                }
-                case "--property" -> cmd.setProperty(getNextParameter(args, i++));
-                case "--mount" -> {
-                    cmd.setMountPath(getNextParameter(args, i++));
-                    cmd.setAction(Command.Action.MOUNT);
-                }
-                case "--sync" -> {
-                    cmd.setSyncPath(getNextParameter(args, i++));
-                    cmd.setAction(Command.Action.SYNC);
-                }
-                case "--ext" -> cmd.setFileExtension(getNextParameter(args, i++));
-                case "--loglevel" -> cmd.setLogLevel(getNextParameter(args, i++));
-                case "--print-xml" -> cmd.setPrintXML(true);
-                case "--help", "-help", "-h", "help" -> cmd.setAction(Command.Action.HELP);
                 case "-n", "--new" -> cmd.setAcceptNewDocument(true);
+                case "--ext" -> cmd.setFileExtension(getNextParameter(args, i++));
+
+                case "--pom" -> cmd.setPom(true);
+                case "--no-read-wiki" -> cmd.setNoReadWiki(true);
+                case "--no-write-wiki" -> cmd.setNoWriteWiki(true);
+                case "--no-mvn-repo-write" -> cmd.setNoMvnRepoWrite(true);
+                case "--no-mvn-repo-read" -> cmd.setNoMvnRepoRead(true);
+                case "--cli-dir" -> cmd.setCliDir(getNextParameter(args, i++));
+                case "--mvn-repo" -> cmd.setMvnRepo(getNextParameter(args, i++));
+
+                case "--user" -> cmd.setUser(getNextParameter(args, i++));
+                case "--pass" -> cmd.setPass(getNextParameter(args, i++));
 
                 default -> throw new CommandException("Unknown option " + args[i] + ". Try --help.");
             }
@@ -146,14 +167,20 @@ public class Arguments
         }
     }
 
-    static void endArgs(Command cmd)
+    static void endArgs(Command cmd) throws CommandException
     {
         if (cmd.action() == null) {
             cmd.setAction(Command.Action.HELP);
         }
 
         if (cmd.action() == Command.Action.SYNC) {
-            cmd.setXmlWriteDir(cmd.syncDataSource());
+            if (cmd.noMvnRepoRead()) {
+                throw new CommandException(
+                    "Not implemented yet. The only supported initial source for sync is the maven repos.");
+            }
+            if (StringUtils.isEmpty(cmd.mvnRepo())) {
+                throw new CommandException("No maven repo specified.");
+            }
         }
 
         if (cmd.logLevel() == null) {
